b6d50d5510cecce0dc726e58b9728719ffde96c4
Sahil Gupte <ovenoboyo@gmail.com>
2023-05-23 10:07:13 +0530

    Update env_config.rs

--- src/env_config.rs
+++ src/env_config.rs
@@ -1,5 +1,5 @@
 pub fn load_env() {
     let app_env = std::env::var("APP_ENV").expect("APP_ENV should be defined");
     println!("APP_ENV: {}", app_env);
-    dotenv::from_filename(format!(".env.{}", app_env)).unwrap();
+    dotenv::from_filename(format!("./.env.{}", app_env)).unwrap();
 }
